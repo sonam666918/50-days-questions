e85dbc7abcca989a9b20466feae09704d2124486
Sonam Yadav <you@example.com>
2025-06-18 21:40:27 +0530

    commit

---
+++ question21.java
@@ -0,0 +1,13 @@
+// reverse a string
+public class question21 {
+    
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String reversed = "";
+
+        for(int i = str.length() - 1; i >= 0; i--){
+            reversed += str.charAt(i);
+        }
+        System.out.println("Reversed: " + reversed);
+    }
+}
