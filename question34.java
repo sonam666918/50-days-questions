6829daff6b946d6f3b1033f62282557fc771c27d
Sonam Yadav <yadavsonam7268@gmail.com>
2025-07-01 23:37:23 +0530

    commit

---
+++ question34.java
@@ -0,0 +1,24 @@
+//check if a string is a rotation of another string 
+public class question34{
+    public static boolean isRotation(String s1,String s2){
+
+        if(s1 == null || s2 == null) return false;
+        if(s1.length() != s2.length()) return false;
+
+        String concatenated = s1 + s2;
+        return concatenated.contains(s2);
+
+    }
+
+    public static void main(String[] args) {
+        String s1 = "particular";
+        String s2 = "arlarpitcu";
+
+        if(isRotation(s1, s2)){
+            System.out.println(s2 + " is a rotation of " + s1);
+        }else{
+            System.out.println(s2 + "is not a rotation of " + s1);
+        }
+    }
+
+}
