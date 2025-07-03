053ccc864fd3ab75aebdda7e746c70c0aa1bd13b
Sonam Yadav <yadavsonam7268@gmail.com>
2025-07-03 19:58:04 +0530

    commit

---
+++ question36.java
@@ -0,0 +1,25 @@
+//transpose of a matrix
+public class question36 {
+    public static void main(String[] args) {
+        int[][] matrix ={
+            {3,4,5},{6,7,8}
+        };
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int [][] transpose = new int[cols][rows];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        for(int i = 0; i < cols; i++){
+            for(int j = 0; j< rows; j++){
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
